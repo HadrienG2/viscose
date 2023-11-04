3c4c10927d70cce411c9c9c6d5b6399d9ddce7d4
Hadrien G. <knights_of_ni@gmx.com>
2023-11-04 14:53:34 +0100

    Extract Job into its own module

---
+++ src/job.rs
@@ -0,0 +1,135 @@
+//! Thread pool job
+
+use crate::{Scope, Work};
+use std::{cell::UnsafeCell, panic::AssertUnwindSafe};
+
+/// [`Work`] that has been prepared for execution by the thread pool
+///
+/// # Safety
+///
+/// Safe use of [`Job`] requires carefully following the following procedure:
+///
+/// - Create a [`Job`] on the stack frame where it will be executed.
+/// - Create a type-erased job handle with `as_dyn()` and submit it for
+///   execution on the thread pool.
+/// - Until the job completion signal is received, do not exit the current stack
+///   frame or interact with the Job in any way, including but not limited to...
+///     - Moving or dropping the job
+///     - Calling any Job method
+/// - Once a job completion signal has been received with Acquire memory
+///   ordering, you may extract the result and propagate panics with `result()`.
+pub(crate) struct Job<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify>(
+    UnsafeCell<JobState<Res, ImplWork, ImplNotify>>,
+);
+//
+impl<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify> Job<Res, ImplWork, ImplNotify> {
+    /// Prepare [`Work`] for execution by the thread pool
+    pub fn new(notify: ImplNotify, work: ImplWork) -> Self {
+        Self(UnsafeCell::new(JobState::Scheduled(notify, work)))
+    }
+
+    /// Create a type-erased handle that can be pushed on a work queue
+    ///
+    /// # Safety
+    ///
+    /// Should only be called once, as preparation for submitting the job to the
+    /// thread pool.
+    pub unsafe fn as_dyn(&mut self) -> DynJob {
+        let state = self.0.get();
+        let state = state.cast::<()>();
+        let run = |state: *mut (), scope: &Scope<'_>| {
+            let state = state.cast::<JobState<Res, ImplWork, ImplNotify>>();
+            // SAFETY: Per `Job` API contract
+            unsafe { (*state).run(scope) };
+        };
+        DynJob { state, run }
+    }
+
+    /// Extract the job result or propagate job panic
+    ///
+    /// # Safety
+    ///
+    /// Should only be called after the job completion notification has been
+    /// received.
+    pub unsafe fn result(mut self) -> Res {
+        match std::mem::replace(self.0.get_mut(), JobState::Collected) {
+            JobState::Scheduled(_, _) | JobState::Running => {
+                panic!("Job result shouldn't be collected before completion notification")
+            }
+            JobState::Finished(result) => match result {
+                Ok(result) => result,
+                Err(payload) => std::panic::resume_unwind(payload),
+            },
+            JobState::Collected => unreachable!(),
+        }
+    }
+}
+
+/// [`Job`] state machine
+enum JobState<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify> {
+    /// Job has not started executing yet
+    Scheduled(ImplNotify, ImplWork),
+
+    /// Job is executing on some worker thread
+    Running,
+
+    /// Job execution is complete and result can be collected
+    Finished(std::thread::Result<Res>),
+
+    /// Job result has been collected
+    Collected,
+}
+//
+impl<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify> JobState<Res, ImplWork, ImplNotify> {
+    /// Run the job
+    ///
+    /// This should only be called once, failure to do so will result in at
+    /// least a panic and likely a full program abort.
+    fn run(&mut self, scope: &Scope<'_>) {
+        let Self::Scheduled(notify, work) = std::mem::replace(self, Self::Running) else {
+            panic!("attempted to execute a Job in an invalid state");
+        };
+        *self = Self::Finished(std::panic::catch_unwind(AssertUnwindSafe(|| work(scope))));
+        notify.notify()
+    }
+}
+
+/// Type-erased handle to a [`Job`]
+pub(crate) struct DynJob {
+    /// Type-erased `&mut JobState<...>` pointer
+    state: *mut (),
+
+    /// Type-erased `JobState<...>::run()` method
+    ///
+    /// The first parameter must be `self.job`.
+    run: fn(*mut (), &Scope<'_>),
+}
+//
+impl DynJob {
+    /// Execute the job
+    ///
+    /// # Safety
+    ///
+    /// See top-level [`Job`] documentation.
+    pub unsafe fn run(self, scope: &Scope<'_>) {
+        (self.run)(self.state, scope)
+    }
+}
+//
+// SAFETY: It is safe to send a DynJob to another thread because inner Work is
+//         safe to send across thread and the Job API contract ensures that the
+//         current thread will not touch the Job in any way until the other
+//         thread is done with the DynJob, which means that for all intents and
+//         purposes we effectively own the inner Work.
+unsafe impl Send for DynJob {}
+
+/// Mechanism to notify the program that a job is done executing
+///
+/// # Safety
+///
+/// No notification should be received until `self.notify()` is called, and the
+/// notification process must feature an `Ordering::Release` memory barrier.
+pub(crate) unsafe trait Notify {
+    /// Send in the notification
+    fn notify(self);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![warn(clippy::print_stdout, clippy::print_stderr, clippy::dbg_macro)]
 
 pub mod flags;
+pub mod job;
 
+use crate::job::{DynJob, Job, Notify};
 use crossbeam::deque::{self, Injector, Steal, Stealer};
 use flags::AtomicFlags;
 use hwlocality::{
@@ -10,7 +12,7 @@ use hwlocality::{
     topology::Topology,
 };
 use std::{
-    cell::{Cell, UnsafeCell},
+    cell::Cell,
     collections::HashMap,
     panic::AssertUnwindSafe,
     sync::{
@@ -660,137 +662,6 @@ unsafe impl Notify for NotifyJoin<'_> {
     }
 }
 
-/// [`Work`] that has been prepared for execution by the thread pool
-///
-/// # Safety
-///
-/// Safe use of [`Job`] requires carefully following the following procedure:
-///
-/// - Create a [`Job`] on the stack frame where it will be executed.
-/// - Create a type-erased job handle with `as_dyn()` and submit it for
-///   execution on the thread pool.
-/// - Until the job completion signal is received, do not exit the current stack
-///   frame or interact with the Job in any way, including but not limited to...
-///     - Moving or dropping the job
-///     - Calling any Job method
-/// - Once a job completion signal has been received with Acquire memory
-///   ordering, you may extract the result and propagate panics with `result()`.
-struct Job<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify>(
-    UnsafeCell<JobState<Res, ImplWork, ImplNotify>>,
-);
-//
-impl<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify> Job<Res, ImplWork, ImplNotify> {
-    /// Prepare [`Work`] for execution by the thread pool
-    pub fn new(notify: ImplNotify, work: ImplWork) -> Self {
-        Self(UnsafeCell::new(JobState::Scheduled(notify, work)))
-    }
-
-    /// Create a type-erased handle that can be pushed on a work queue
-    ///
-    /// # Safety
-    ///
-    /// Should only be called once, as preparation for submitting the job to the
-    /// thread pool.
-    pub unsafe fn as_dyn(&mut self) -> DynJob {
-        let state = self.0.get();
-        let state = state.cast::<()>();
-        let run = |state: *mut (), scope: &Scope<'_>| {
-            let state = state.cast::<JobState<Res, ImplWork, ImplNotify>>();
-            // SAFETY: Per `Job` API contract
-            unsafe { (*state).run(scope) };
-        };
-        DynJob { state, run }
-    }
-
-    /// Extract the job result or propagate job panic
-    ///
-    /// # Safety
-    ///
-    /// Should only be called after the job completion notification has been
-    /// received.
-    pub unsafe fn result(mut self) -> Res {
-        match std::mem::replace(self.0.get_mut(), JobState::Collected) {
-            JobState::Scheduled(_, _) | JobState::Running => {
-                panic!("Job result shouldn't be collected before completion notification")
-            }
-            JobState::Finished(result) => match result {
-                Ok(result) => result,
-                Err(payload) => std::panic::resume_unwind(payload),
-            },
-            JobState::Collected => unreachable!(),
-        }
-    }
-}
-
-/// [`Job`] state machine
-enum JobState<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify> {
-    /// Job has not started executing yet
-    Scheduled(ImplNotify, ImplWork),
-
-    /// Job is executing on some worker thread
-    Running,
-
-    /// Job execution is complete and result can be collected
-    Finished(std::thread::Result<Res>),
-
-    /// Job result has been collected
-    Collected,
-}
-//
-impl<Res: Send, ImplWork: Work<Res>, ImplNotify: Notify> JobState<Res, ImplWork, ImplNotify> {
-    /// Run the job
-    ///
-    /// This should only be called once, failure to do so will result in at
-    /// least a panic and likely a full program abort.
-    fn run(&mut self, scope: &Scope<'_>) {
-        let Self::Scheduled(notify, work) = std::mem::replace(self, Self::Running) else {
-            panic!("attempted to execute a Job in an invalid state");
-        };
-        *self = Self::Finished(std::panic::catch_unwind(AssertUnwindSafe(|| work(scope))));
-        notify.notify()
-    }
-}
-
-/// Type-erased handle to a [`Job`]
-struct DynJob {
-    /// Type-erased `&mut JobState<...>` pointer
-    state: *mut (),
-
-    /// Type-erased `JobState<...>::run()` method
-    ///
-    /// The first parameter must be `self.job`.
-    run: fn(*mut (), &Scope<'_>),
-}
-//
-impl DynJob {
-    /// Execute the job
-    ///
-    /// # Safety
-    ///
-    /// See top-level [`Job`] documentation.
-    pub unsafe fn run(self, scope: &Scope<'_>) {
-        (self.run)(self.state, scope)
-    }
-}
-//
-// SAFETY: It is safe to send a DynJob to another thread because inner Work is
-//         safe to send across thread and the Job API contract ensures that the
-//         current thread will not touch the Job in any way until the other
-//         thread is done with the DynJob, which means that for all intents and
-//         purposes we effectively own the inner Work.
-unsafe impl Send for DynJob {}
-
-/// Mechanism to notify the program that a job is done executing
-///
-/// # Safety
-///
-/// No notification should be received until `self.notify()` is called, and the
-/// notification process must feature an `Ordering::Release` memory barrier.
-unsafe trait Notify {
-    /// Send in the notification
-    fn notify(self);
-}
-
 /// Function that can be scheduled for execution by the thread pool
 ///
 /// The input [`Scope`] allows the scheduled work to interact with the thread
@@ -804,8 +675,8 @@ where
 {
 }
 
-/// Translate unwinding panics to aborts
-pub fn abort_on_panic<R>(f: impl FnOnce() -> R) -> R {
+/// Translate unwinding panics into aborts
+fn abort_on_panic<R>(f: impl FnOnce() -> R) -> R {
     match std::panic::catch_unwind(AssertUnwindSafe(f)) {
         Ok(result) => result,
         Err(_) => std::process::abort(),
